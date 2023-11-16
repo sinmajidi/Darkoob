26c4d873dff07d41331f4c36db56564ef3d56ea1
Sina Majidi <sin.majidi@gmail.com>
2023-11-16 02:36:15 -0800

    i changed readme to add ORM

--- Darkoob_sqlite.py
+++ Darkoob_sqlite.py
@@ -10,12 +10,32 @@ class DarkoobSQLIT:
         self.folder_path = os.path.abspath(folder_path)
 
     def create_table(self, table_name='default_table', **columns):
+        """
+                Create a table in the database.
+
+                Args:
+                    table_name (str): Name of the table.
+                    columns (dict): Dictionary with column names and types.
+
+                Returns:
+                    None
+        """
         columns_str = ', '.join([f'{key} {value}' for key, value in columns.items()])
         create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_str})"
         self.cursor.execute(create_table_query)
         self.conn.commit()
 
     def insert(self, table_name, **data):
+        """
+                Insert data into the table.
+
+                Args:
+                    table_name (str): Name of the table.
+                    data (dict): Dictionary with column names and values.
+
+                Returns:
+                    None
+        """
         columns_str = ', '.join(data.keys())
         values_str = ', '.join([f'"{value}"' for value in data.values()])
         insert_query = f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str})"
@@ -23,12 +43,33 @@ class DarkoobSQLIT:
         self.conn.commit()
 
     def select(self, table_name, **condition):
+        """
+                Select data from the table based on conditions.
+
+                Args:
+                    table_name (str): Name of the table.
+                    condition (dict): Dictionary with column names and values for the WHERE clause.
+
+                Returns:
+                    list: List of tuples representing the selected rows.
+        """
         condition_str = ' AND '.join([f'{key}="{value}"' for key, value in condition.items()])
         select_query = f"SELECT * FROM {table_name} WHERE {condition_str}"
         self.cursor.execute(select_query)
         return self.cursor.fetchall()
 
     def update(self, table_name, data, **condition):
+        """
+                Update data in the table based on conditions.
+
+                Args:
+                    table_name (str): Name of the table.
+                    data (dict): Dictionary with column names and values to be updated.
+                    condition (dict): Dictionary with column names and values for the WHERE clause.
+
+                Returns:
+                    None
+        """
         set_str = ', '.join([f'{key}="{value}"' for key, value in data.items()])
         condition_str = ' AND '.join([f'{key}="{value}"' for key, value in condition.items()])
         update_query = f"UPDATE {table_name} SET {set_str} WHERE {condition_str}"
@@ -36,12 +77,28 @@ class DarkoobSQLIT:
         self.conn.commit()
 
     def delete(self, table_name, **condition):
+        """
+                Delete data from the table based on conditions.
+
+                Args:
+                    table_name (str): Name of the table.
+                    condition (dict): Dictionary with column names and values for the WHERE clause.
+
+                Returns:
+                    None
+        """
         condition_str = ' AND '.join([f'{key}="{value}"' for key, value in condition.items()])
         delete_query = f"DELETE FROM {table_name} WHERE {condition_str}"
         self.cursor.execute(delete_query)
         self.conn.commit()
 
     def close_connection(self):
+        """
+               Close the database connection.
+
+               Returns:
+                   None
+        """
         self.conn.close()
 
 
---
+++ README.md
@@ -0,0 +1,76 @@
+# Darkoob Framework
+
+Welcome to Darkoob Framework by AFRA IOT! 🚀
+
+Darkoob is a Python backend framework designed to simplify web development 
+and provide developers with powerful tools for building robust web applications. 
+It allows you to define routes easily and handle different HTTP methods such as GET, 
+POST, PUT, and DELETE.
+
+## Features
+
+- Simple and intuitive routing system
+- Support for multiple HTTP methods: GET, POST, PUT, DELETE
+- Template rendering using Jinja2
+- Basic error handling for 404 and 405 responses
+
+## Getting Started
+
+```bash
+# Install the required dependencies listed in `requirements.txt` using pip:
+pip install -r requirements.txt
+
+# Define your routes in `routes.py` and implement corresponding view functions in `views.py`.
+
+# Run your Darkoob server using the following command:
+python routes.py
+
+Visit http://127.0.0.1:8080 in your web browser to see your Darkoob application in action!
+
+Project Structure
+# Define your application routes and start the server.
+- `routes.py`
+
+# Implement your route functions for handling different paths and HTTP methods.
+- `views.py`
+
+# Core framework file containing the server logic and request handling.
+- `Darkoob.py`
+
+# Directory to store your HTML templates.
+- `templates/`
+
+DarkoobSQLITE
+Integrate DarkoobSQLITE for easy interaction with SQLite databases in Darkoob.
+
+# Example usage of DarkoobSQLITE
+from Darkoob_sqlite import DarkoobSQLIT
+
+# Initialize DarkoobSQLITE
+orm = DarkoobSQLIT()
+
+# Create a table named 'users'
+orm.create_table(table_name='users', id='INTEGER PRIMARY KEY', name='TEXT', age='INTEGER')
+
+# Insert data into the 'users' table
+orm.insert('users', name='John', age=25)
+
+# Select data from the 'users' table where name is 'John'
+print(orm.select('users', name='John'))
+
+# Update data in the 'users' table where name is 'John'
+orm.update('users', {'age': 30}, name='John')
+
+# Delete data from the 'users' table where name is 'John'
+orm.delete('users', name='John')
+
+# Close the database connection
+orm.close_connection()
+
+
+Contributing
+Darkoob is an open-source project, and we welcome contributions from the community. If you have any suggestions, bug reports, or feature requests, please create an issue or submit a pull request.
+
+Happy coding! 😊
+
+For more information, visit afraiot.org.
